e4750f4ecd2eac192d4c5efc1e4b4b350875f917
Rich Beddington <rbeddington@spotify.com>
2020-07-13 10:35:33 +0100

    Add date to info

--- client/src/Viewer.tsx
+++ client/src/Viewer.tsx
@@ -86,16 +86,22 @@ const Viewer = ({ date, hasOrientationPermission }: ViewerProps) => {
       <LoaderWrapper show={loading}>
         <Loader style={{ marginTop: 70 }} />
       </LoaderWrapper>
-      <InfoWrap>
-        <p>Distance from previous earth position: {distance}</p>
-        <p>
-          The <Color color="white">white</Color> dot is the moon in its current
-          position, the <Color color="yellow">yellow</Color> is the sun in its
-          current position and the <Color color="#88aaff">light blue</Color> is
-          the earth position at the date specified. The{" "}
-          <Color color={Colors.strong.toString()}>pink</Color> line is North.
-        </p>
-      </InfoWrap>
+      {!loading && (
+        <InfoWrap>
+          <p>
+            Distance from earth position on {new Date(date).toLocaleString()} is{" "}
+            {distance}.
+          </p>
+          <p>
+            The <Color color="white">white</Color> dot is the moon in its
+            current position, the <Color color="yellow">yellow</Color> is the
+            sun in its current position and the{" "}
+            <Color color="#88aaff">light blue</Color> is the earth position at
+            the date specified. The{" "}
+            <Color color={Colors.strong.toString()}>pink</Color> line is North.
+          </p>
+        </InfoWrap>
+      )}
     </Wrapper>
   );
 };
